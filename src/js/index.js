cf51f9c291d6f54e6122530f9c364069de437359
dartvalerius <filippenkov.valeriy@gmail.com>
2024-04-13 19:00:34 +0300

    подготовлена разметка

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
-import * as bootstrap from 'bootstrap'
+const cart = document.querySelector(".cart");
+document.querySelector("#cartOpen").addEventListener("click", () => cart.showModal());
+document.querySelector('.cart-btn-close').addEventListener("click", () => cart.close());
+
+
+
+const view = document.querySelector(".view");
+document.querySelector(".good-card__img").addEventListener("click", () => view.showModal());
+document.querySelector('.view-btn-close').addEventListener("click", () => view.close());
+
