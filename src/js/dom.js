4e3ae4cbee33e30dc4289c2c97e8c460e5fd0703
dartvalerius <filippenkov.valeriy@gmail.com>
2024-04-15 21:30:37 +0300

    реализовано отображение товаров

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,10 +1,31 @@
+import goodImg from "../img/goods/*/*.jpg";
+
 // создание карточки товара
 const createGoodCard = (good) => {
+    const imgContainer = createDivContainer("good-card__img", [createGoodImage(good)]);
+    const titleContainer = createDivContainer("good-card__title", [document.createTextNode(`${good.name}`)]);
+    
+    const discountContainer = createDivContainer("good-card__discount", [document.createTextNode(`-${good.discount}%`)]);
+    if(good.discount === 0) discountContainer.style.display = "none";
+    
+    const buttonContainer = createDivContainer("good-card__btn", [createDivContainer("btn-cart", [document.createTextNode("В корзину")])])
+    
+    const priceContainer = createDivContainer("good-card__price");
+    if(good.discount > 0) {
+        priceContainer.append(createDivContainer("full-price", [document.createTextNode(`${good.price} р.`)]),
+            createDivContainer("discount", [document.createTextNode(`${good.price - good.price * good.discount / 100} р.`)]));
+    } else {
+        priceContainer.append(createDivContainer("price", [document.createTextNode(`${good.price} р.`)]));
+    }
 
+    const cartContainer = createDivContainer("good-card", [imgContainer, titleContainer, priceContainer, buttonContainer, discountContainer]);
+    cartContainer.setAttribute("data-cart-id", good.id)
+
+    return cartContainer;
 }
 
 // создание слайда товара по скидке
-const createGoodDiscoutSlide = (good) => {
+const createGoodDiscountSlide = (good) => {
 
 }
 
@@ -21,14 +42,14 @@ const renderCards = (goods) => {
     goods.forEach(good => goodCardWrapper.append(createGoodCard(good)));
 }
 
-// отризовка слайдов
+// отрисовка слайдов
 const renderDiscountSlides = (goods) => {
     const goodDiscountSlider = document.querySelector(".swiper-wrapper");
     goodDiscountSlider.innerHTML = "";
 
     goods
         .filter(good => good.discount > 0)
-        .forEach(good => goodDiscountSlider.append(createGoodDiscoutSlide(good)));
+        .forEach(good => goodDiscountSlider.append(createGoodDiscountSlide(good)));
 }
 
 // отрисовка козины
@@ -43,8 +64,24 @@ const renderCart = (goods) => {
 const createGoodImage = (good, isBig = false) => {
     const img = document.createElement("img");
 
-    img.setAttribute("src", `img/goods/${id}/${isBig ? good.id + "-big" : good.id}.jpg`);
+    img.setAttribute("src", goodImg[good.id][isBig ? good.id + "-big" : good.id]);
     img.setAttribute("alt", good.name);
 
     return img;
+}
+
+// создать контейнер
+const createDivContainer = (className, appendElements) => {
+    const div = document.createElement("div");
+
+    if(className) div.setAttribute("class", className);
+    if(appendElements) div.append(...appendElements);
+
+    return div;
+}
+
+export {
+    renderCards,
+    renderCart,
+    renderDiscountSlides
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import { listAsync, goods, cartItems } from "./data.js";
+import { renderCards, renderCart, renderDiscountSlides} from "./dom.js"
+import goodImg from "../img/goods/*/*.jpg";
 
 const cart = document.querySelector(".cart");
 document.querySelector("#cartOpen").addEventListener("click", () => cart.showModal());
@@ -13,9 +15,11 @@ document.querySelector('.view-btn-close').addEventListener("click", () => view.c
 (async function(){
     try {
         await listAsync();
-        console.log(goods);
+        console.log(goodImg)
+        renderCards(goods);
     }
     catch (e) {
         console.log("Ошибка получения товаров")
+        console.log(e);
     }
 })()
