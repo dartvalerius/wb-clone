b1ad490812dc07dd140aab49cc5a61897233f56c
Filippenkov Valeriy <v.filippenkov@defin.by>
2024-04-18 11:02:31 +0300

    исправлена работа слайдера

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import { listAsync, goods, cartData } from "./data.js";
 import { renderLoadMessage, renderCards, renderCart, renderDiscountSlides} from "./dom.js";
 import { regViewClose, regCartDialogHandlers, regSearchHandler } from "./handlers.js";
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
 (async function(){
     try {
@@ -15,6 +17,25 @@ import { regViewClose, regCartDialogHandlers, regSearchHandler } from "./handler
         regViewClose();
         regCartDialogHandlers();
         regSearchHandler();
+
+        const swiper = new Swiper('.swiper', {
+            modules: [Navigation, Pagination],
+            // Optional parameters
+            loop: true,
+          
+            // If we need pagination
+            pagination: {
+              el: '.swiper-pagination',
+              clickable: true,
+            },
+          
+            // Navigation arrows
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+              disabledClass: 'disabled_swiper_button'
+            },
+          });
     }
     catch (e) {
         console.error(e);
